e84657921a400572de43f8463eaec5c535a4327c
Ray_kuo <raykuo7@gmail.com>
2024-10-21 11:16:24 +0800

    10211100

--- Python/lesson5_1021/lesson_0.py
+++ Python/lesson5_1021/lesson_0.py
@@ -2,17 +2,21 @@ from ttkthemes import ThemedTk
 from tkinter import ttk
 
 
-class Window(ThemedTk):
+class Window(ThemedTk):                 
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.title('使用ttk的套件')
         style = ttk.Style(self)        
 
         topFrame = ttk.Frame(self,borderwidth=1,relief='groove',)
-        btn1 = ttk.Button(topFrame,text="按鈕1", command=self.userclick1) # commend 為按鈕控制
-        btn1.pack(side='left',expand=True,fill='x',padx=10)
+        self.btn1 = ttk.Button(topFrame,text="按鈕1", command=self.userclick1) # commend 為按鈕控制
+        self.btn1.pack(side='left',expand=True,fill='x',padx=10)
+      
         btn2 = ttk.Button(topFrame,text="按鈕2", command=self.userclick2)
+        btn2.bind('<ButtonRelease>', self.btn2_click )
         btn2.pack(side='left',expand=True,fill='x')
+
+        
         btn3 = ttk.Button(topFrame,text="按鈕3", command=self.userclick3)
         btn3.pack(side='left',expand=True,fill='x',padx=10)
         topFrame.pack(padx=10,pady=(10,0),ipadx=10,ipady=10,expand=True,fill='x')
@@ -28,7 +32,9 @@ class Window(ThemedTk):
         f3.pack(side='left', padx=10, expand=True,fill='both')
         
         
-        btna1 = ttk.Button(f1,text="按鈕1")
+        btna1 = ttk.Button(f1,text="按鈕1")             # link
+        btna1.bind('<ButtonRelease>', self.btna1_click )
+
         btna1.pack(fill='x', padx=10, pady=(5, 5), ipady=50)
         btna2 = ttk.Button(f1,text="按鈕2")
         btna2.pack(fill='x', padx=10, pady=(5, 5), ipady=25)
@@ -53,10 +59,21 @@ class Window(ThemedTk):
 
     def userclick1(self):
         print('hi but1')
+        self.btn1.configure(text='pressed!')
     def userclick2(self):
         print('hi but2')
     def userclick3(self):
         print('hi but3')
+
+    def btna1_click(self, event):       # link
+        print('btna1_click')
+        print(event)
+        print(event.widget.configure(text='pressed!'))  
+    
+    def btn2_click(self, event):
+        print('btn2_click')
+    
+
 def main():
     window = Window(theme="arc")
     window.mainloop()
